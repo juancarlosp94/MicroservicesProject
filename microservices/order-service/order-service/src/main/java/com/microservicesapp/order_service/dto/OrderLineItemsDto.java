93b2c3bef6bbf2b45bdecf646709fee0c917c5a2
JUAN_PERDIGON <juanc9410@gmail.com>
2024-07-31 20:40:45 -0500

    Order service first requests

---
+++ microservices/order-service/order-service/src/main/java/com/microservicesapp/order_service/dto/OrderLineItemsDto.java
@@ -0,0 +1,17 @@
+package com.microservicesapp.order_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItemsDto {
+    private Long id;
+    private String skuCode; //"sku, stock keeping unit"
+    private BigDecimal price;
+    private Integer quantity;
+}
